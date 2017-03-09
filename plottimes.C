c63ab1a0105cbd147f7ae1806a0d6ce260ebd285
Alessandro Morda <alessandro.morda@desy.de>
2017-03-09 10:06:50 +0100

    changes to store also the dataset in histogram format

--- plottimes.C
+++ plottimes.C
@@ -6,7 +6,7 @@ using namespace std;
 #include <TH1F.h>
 #include <TCanvas.h>
 
-void make_flat_ntuple(TString input_path,TString rfile){
+void make_flat_ntuple(int pos, double amplitude_cut=10, float xmin=22., float xmax=25, TString input_path,TString rfile){
   TString out_path="flat_ntuples/";
   double Amplitude (99);
   double Time (-999);
@@ -37,13 +37,45 @@ void make_flat_ntuple(TString input_path,TString rfile){
   tree->SetBranchAddress("times", &(times[0][0]));
   tree->SetBranchAddress("amplitudes", &(amplitudes[0][0]));
 
-  //TH1F* htimes = new TH1F(Form("fiber0-%d-%d",pos,ch), Form("fiber0-%d-%d",pos,ch), 200, xmin, xmax);
+  TH1F* htimes_0 = new TH1F(Form("fiber0-%d-0",pos), Form("fiber0-%d-0",pos), 200, xmin, xmax);
+  TH1F* htimes_1 = new TH1F(Form("fiber0-%d-1",pos), Form("fiber0-%d-1",pos), 200, xmin, xmax);
+  TH1F* htimes_2 = new TH1F(Form("fiber0-%d-2",pos), Form("fiber0-%d-2",pos), 200, xmin, xmax);
+  TH1F* htimes_3 = new TH1F(Form("fiber0-%d-3",pos), Form("fiber0-%d-3",pos), 200, xmin, xmax);
+  TH1F* htimes_4 = new TH1F(Form("fiber0-%d-4",pos), Form("fiber0-%d-4",pos), 200, xmin, xmax);
+  TH1F* htimes_5 = new TH1F(Form("fiber0-%d-5",pos), Form("fiber0-%d-5",pos), 200, xmin, xmax);
+  TH1F* htimes_6 = new TH1F(Form("fiber0-%d-6",pos), Form("fiber0-%d-6",pos), 200, xmin, xmax);
+  TH1F* htimes_7 = new TH1F(Form("fiber0-%d-7",pos), Form("fiber0-%d-7",pos), 200, xmin, xmax);
+  TH1F* htimes_8 = new TH1F(Form("fiber0-%d-8",pos), Form("fiber0-%d-8",pos), 200, xmin, xmax);
+  TH1F* htimes_9 = new TH1F(Form("fiber0-%d-9",pos), Form("fiber0-%d-9",pos), 200, xmin, xmax);
+  TH1F* htimes_10 = new TH1F(Form("fiber0-%d-10",pos), Form("fiber0-%d-10",pos), 200, xmin, xmax);
+  TH1F* htimes_11 = new TH1F(Form("fiber0-%d-11",pos), Form("fiber0-%d-11",pos), 200, xmin, xmax);
+  TH1F* htimes_12 = new TH1F(Form("fiber0-%d-12",pos), Form("fiber0-%d-12",pos), 200, xmin, xmax);
+  TH1F* htimes_13 = new TH1F(Form("fiber0-%d-13",pos), Form("fiber0-%d-13",pos), 200, xmin, xmax);
+  TH1F* htimes_14 = new TH1F(Form("fiber0-%d-14",pos), Form("fiber0-%d-14",pos), 200, xmin, xmax);
+  TH1F* htimes_15 = new TH1F(Form("fiber0-%d-15",pos), Form("fiber0-%d-15",pos), 200, xmin, xmax);
 
   for (int k=0; k<tree->GetEntries(); ++k) {
     tree->GetEntry(k);
     for (int l=0; l<nchannels; ++l) {
       for (int j=0; j<ntimes[l]; ++j) {
-	//if(amplitudes[l][j]<-amplitude_cut) htimes->Fill(times[l][j]/*-14*/);
+	if(amplitudes[l][j]<-amplitude_cut){
+	  if(channels[l]==0) htimes_0->Fill(times[l][j]);
+	  if(channels[l]==1) htimes_1->Fill(times[l][j]);
+	  if(channels[l]==2) htimes_2->Fill(times[l][j]);
+	  if(channels[l]==3) htimes_3->Fill(times[l][j]);
+	  if(channels[l]==4) htimes_4->Fill(times[l][j]);
+	  if(channels[l]==5) htimes_5->Fill(times[l][j]);
+	  if(channels[l]==6) htimes_6->Fill(times[l][j]);
+	  if(channels[l]==7) htimes_7->Fill(times[l][j]);
+	  if(channels[l]==8) htimes_8->Fill(times[l][j]);
+	  if(channels[l]==9) htimes_9->Fill(times[l][j]);
+	  if(channels[l]==10) htimes_10->Fill(times[l][j]);
+	  if(channels[l]==11) htimes_11->Fill(times[l][j]);
+	  if(channels[l]==12) htimes_12->Fill(times[l][j]);
+	  if(channels[l]==13) htimes_13->Fill(times[l][j]);
+	  if(channels[l]==14) htimes_14->Fill(times[l][j]);
+	  if(channels[l]==15) htimes_15->Fill(times[l][j]);
+	}
 	Amplitude=amplitudes[l][j];
 	Time=times[l][j];
 	Channel=channels[l];
@@ -55,6 +87,22 @@ void make_flat_ntuple(TString input_path,TString rfile){
 
   f_data->cd();
   tree_input->Write();
+  htimes_0->Write();
+  htimes_1->Write();
+  htimes_2->Write();
+  htimes_3->Write();
+  htimes_4->Write();
+  htimes_5->Write();
+  htimes_6->Write();
+  htimes_7->Write();
+  htimes_8->Write();
+  htimes_9->Write();
+  htimes_10->Write();
+  htimes_11->Write();
+  htimes_12->Write();
+  htimes_13->Write();
+  htimes_14->Write();
+  htimes_15->Write();
   f_data->Close();
   delete f_data;
 

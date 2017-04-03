88f2cade1b7a65f698e7097c83bbd9d78a576e58
Alessandro Morda <alessandro.morda@desy.de>
2017-04-03 13:32:12 +0200

    added further plots

--- Analyse_ScanTune.C
+++ Analyse_ScanTune.C
@@ -314,6 +314,85 @@ void Analyse_ScanTune(){
   DELTA_HSP_LSP_T79->SetMarkerStyle(20);DELTA_HSP_LSP_T79->SetMarkerColor(13);        DELTA_HSP_LSP_T79->SetMarkerSize(2);
   DELTA_HSP_LSP_T81->SetMarkerStyle(20);DELTA_HSP_LSP_T81->SetMarkerColor(3);         DELTA_HSP_LSP_T81->SetMarkerSize(2);
   DELTA_HSP_LSP_T795->SetMarkerStyle(20);DELTA_HSP_LSP_T795->SetMarkerColor(13);        DELTA_HSP_LSP_T795->SetMarkerSize(2);
+
+
+
+
+
+  
+  TGraphErrors* SIGMA_H_B_0_T80 = (TGraphErrors*)f_80->Get("SIGMA_H_B_0");SIGMA_H_B_0_T80->SetName("SIGMA_H_B_0_T80");SIGMA_H_B_0_T80->SetTitle("SIGMA_H_B_0_T80");
+  TGraphErrors* SIGMA_H_B_0_T82 = (TGraphErrors*)f_82->Get("SIGMA_H_B_0");SIGMA_H_B_0_T82->SetName("SIGMA_H_B_0_T82");SIGMA_H_B_0_T82->SetTitle("SIGMA_H_B_0_T82");
+  TGraphErrors* SIGMA_H_B_0_T84 = (TGraphErrors*)f_84->Get("SIGMA_H_B_0");SIGMA_H_B_0_T84->SetName("SIGMA_H_B_0_T84");SIGMA_H_B_0_T84->SetTitle("SIGMA_H_B_0_T84");
+  TGraphErrors* SIGMA_H_B_0_T86 = (TGraphErrors*)f_86->Get("SIGMA_H_B_0");SIGMA_H_B_0_T86->SetName("SIGMA_H_B_0_T86");SIGMA_H_B_0_T86->SetTitle("SIGMA_H_B_0_T86");
+  TGraphErrors* SIGMA_H_B_0_T88 = (TGraphErrors*)f_88->Get("SIGMA_H_B_0");SIGMA_H_B_0_T88->SetName("SIGMA_H_B_0_T88");SIGMA_H_B_0_T88->SetTitle("SIGMA_H_B_0_T88");
+  TGraphErrors* SIGMA_H_B_0_T90 = (TGraphErrors*)f_90->Get("SIGMA_H_B_0");SIGMA_H_B_0_T90->SetName("SIGMA_H_B_0_T90");SIGMA_H_B_0_T90->SetTitle("SIGMA_H_B_0_T90");
+  TGraphErrors* SIGMA_H_B_0_T78 = (TGraphErrors*)f_78->Get("SIGMA_H_B_0");SIGMA_H_B_0_T78->SetName("SIGMA_H_B_0_T78");SIGMA_H_B_0_T78->SetTitle("SIGMA_H_B_0_T78");
+  TGraphErrors* SIGMA_H_B_0_T76 = (TGraphErrors*)f_76->Get("SIGMA_H_B_0");SIGMA_H_B_0_T76->SetName("SIGMA_H_B_0_T76");SIGMA_H_B_0_T76->SetTitle("SIGMA_H_B_0_T76");
+  TGraphErrors* SIGMA_H_B_0_T74 = (TGraphErrors*)f_74->Get("SIGMA_H_B_0");SIGMA_H_B_0_T74->SetName("SIGMA_H_B_0_T74");SIGMA_H_B_0_T74->SetTitle("SIGMA_H_B_0_T74");
+  TGraphErrors* SIGMA_H_B_0_T72 = (TGraphErrors*)f_72->Get("SIGMA_H_B_0");SIGMA_H_B_0_T72->SetName("SIGMA_H_B_0_T72");SIGMA_H_B_0_T72->SetTitle("SIGMA_H_B_0_T72");
+  TGraphErrors* SIGMA_H_B_0_T73 = (TGraphErrors*)f_73->Get("SIGMA_H_B_0");SIGMA_H_B_0_T73->SetName("SIGMA_H_B_0_T73");SIGMA_H_B_0_T73->SetTitle("SIGMA_H_B_0_T73");
+  TGraphErrors* SIGMA_H_B_0_T75 = (TGraphErrors*)f_75->Get("SIGMA_H_B_0");SIGMA_H_B_0_T75->SetName("SIGMA_H_B_0_T75");SIGMA_H_B_0_T75->SetTitle("SIGMA_H_B_0_T75");
+  TGraphErrors* SIGMA_H_B_0_T77 = (TGraphErrors*)f_77->Get("SIGMA_H_B_0");SIGMA_H_B_0_T77->SetName("SIGMA_H_B_0_T77");SIGMA_H_B_0_T77->SetTitle("SIGMA_H_B_0_T77");
+  TGraphErrors* SIGMA_H_B_0_T79 = (TGraphErrors*)f_79->Get("SIGMA_H_B_0");SIGMA_H_B_0_T79->SetName("SIGMA_H_B_0_T79");SIGMA_H_B_0_T79->SetTitle("SIGMA_H_B_0_T79");
+  TGraphErrors* SIGMA_H_B_0_T81 = (TGraphErrors*)f_81->Get("SIGMA_H_B_0");SIGMA_H_B_0_T81->SetName("SIGMA_H_B_0_T81");SIGMA_H_B_0_T81->SetTitle("SIGMA_H_B_0_T81");
+  TGraphErrors* SIGMA_H_B_0_T795 = (TGraphErrors*)f_795->Get("SIGMA_H_B_0");SIGMA_H_B_0_T795->SetName("SIGMA_H_B_0_T795");SIGMA_H_B_0_T795->SetTitle("SIGMA_H_B_0_T795");
+
+  // SIGMA_H_B_0_T77->SetMarkerStyle(20);SIGMA_H_B_0_T77->SetMarkerColor(1);         SIGMA_H_B_0_T77->SetMarkerSize(2);
+  SIGMA_H_B_0_T80->SetMarkerStyle(20);SIGMA_H_B_0_T80->SetMarkerColor(9);         SIGMA_H_B_0_T80->SetMarkerSize(2);
+  SIGMA_H_B_0_T82->SetMarkerStyle(20);SIGMA_H_B_0_T82->SetMarkerColor(2);         SIGMA_H_B_0_T82->SetMarkerSize(2);
+  SIGMA_H_B_0_T84->SetMarkerStyle(20);SIGMA_H_B_0_T84->SetMarkerColor(4);         SIGMA_H_B_0_T84->SetMarkerSize(2);
+  SIGMA_H_B_0_T86->SetMarkerStyle(20);SIGMA_H_B_0_T86->SetMarkerColor(8);         SIGMA_H_B_0_T86->SetMarkerSize(2);
+  SIGMA_H_B_0_T88->SetMarkerStyle(20);SIGMA_H_B_0_T88->SetMarkerColor(kOrange-2); SIGMA_H_B_0_T88->SetMarkerSize(2);
+  SIGMA_H_B_0_T90->SetMarkerStyle(20);SIGMA_H_B_0_T90->SetMarkerColor(11);        SIGMA_H_B_0_T90->SetMarkerSize(2);
+  SIGMA_H_B_0_T78->SetMarkerStyle(20);SIGMA_H_B_0_T78->SetMarkerColor(7);         SIGMA_H_B_0_T78->SetMarkerSize(2);
+  SIGMA_H_B_0_T76->SetMarkerStyle(20);SIGMA_H_B_0_T76->SetMarkerColor(6);         SIGMA_H_B_0_T76->SetMarkerSize(2);
+  SIGMA_H_B_0_T74->SetMarkerStyle(20);SIGMA_H_B_0_T74->SetMarkerColor(5);         SIGMA_H_B_0_T74->SetMarkerSize(2);
+  SIGMA_H_B_0_T72->SetMarkerStyle(20);SIGMA_H_B_0_T72->SetMarkerColor(12);        SIGMA_H_B_0_T72->SetMarkerSize(2);
+  SIGMA_H_B_0_T73->SetMarkerStyle(20);SIGMA_H_B_0_T73->SetMarkerColor(3);         SIGMA_H_B_0_T73->SetMarkerSize(2);
+  SIGMA_H_B_0_T75->SetMarkerStyle(20);SIGMA_H_B_0_T75->SetMarkerColor(13);        SIGMA_H_B_0_T75->SetMarkerSize(2);
+  SIGMA_H_B_0_T77->SetMarkerStyle(20);SIGMA_H_B_0_T77->SetMarkerColor(3);         SIGMA_H_B_0_T77->SetMarkerSize(2);
+  SIGMA_H_B_0_T79->SetMarkerStyle(20);SIGMA_H_B_0_T79->SetMarkerColor(13);        SIGMA_H_B_0_T79->SetMarkerSize(2);
+  SIGMA_H_B_0_T81->SetMarkerStyle(20);SIGMA_H_B_0_T81->SetMarkerColor(3);         SIGMA_H_B_0_T81->SetMarkerSize(2);
+  SIGMA_H_B_0_T795->SetMarkerStyle(20);SIGMA_H_B_0_T795->SetMarkerColor(13);        SIGMA_H_B_0_T795->SetMarkerSize(2);
+
+
+  
+
+  
+  TGraphErrors* DELTA_H_B_0_T80 = (TGraphErrors*)f_80->Get("DELTA_H_B_0");DELTA_H_B_0_T80->SetName("DELTA_H_B_0_T80");DELTA_H_B_0_T80->SetTitle("DELTA_H_B_0_T80");
+  TGraphErrors* DELTA_H_B_0_T82 = (TGraphErrors*)f_82->Get("DELTA_H_B_0");DELTA_H_B_0_T82->SetName("DELTA_H_B_0_T82");DELTA_H_B_0_T82->SetTitle("DELTA_H_B_0_T82");
+  TGraphErrors* DELTA_H_B_0_T84 = (TGraphErrors*)f_84->Get("DELTA_H_B_0");DELTA_H_B_0_T84->SetName("DELTA_H_B_0_T84");DELTA_H_B_0_T84->SetTitle("DELTA_H_B_0_T84");
+  TGraphErrors* DELTA_H_B_0_T86 = (TGraphErrors*)f_86->Get("DELTA_H_B_0");DELTA_H_B_0_T86->SetName("DELTA_H_B_0_T86");DELTA_H_B_0_T86->SetTitle("DELTA_H_B_0_T86");
+  TGraphErrors* DELTA_H_B_0_T88 = (TGraphErrors*)f_88->Get("DELTA_H_B_0");DELTA_H_B_0_T88->SetName("DELTA_H_B_0_T88");DELTA_H_B_0_T88->SetTitle("DELTA_H_B_0_T88");
+  TGraphErrors* DELTA_H_B_0_T90 = (TGraphErrors*)f_90->Get("DELTA_H_B_0");DELTA_H_B_0_T90->SetName("DELTA_H_B_0_T90");DELTA_H_B_0_T90->SetTitle("DELTA_H_B_0_T90");
+  TGraphErrors* DELTA_H_B_0_T78 = (TGraphErrors*)f_78->Get("DELTA_H_B_0");DELTA_H_B_0_T78->SetName("DELTA_H_B_0_T78");DELTA_H_B_0_T78->SetTitle("DELTA_H_B_0_T78");
+  TGraphErrors* DELTA_H_B_0_T76 = (TGraphErrors*)f_76->Get("DELTA_H_B_0");DELTA_H_B_0_T76->SetName("DELTA_H_B_0_T76");DELTA_H_B_0_T76->SetTitle("DELTA_H_B_0_T76");
+  TGraphErrors* DELTA_H_B_0_T74 = (TGraphErrors*)f_74->Get("DELTA_H_B_0");DELTA_H_B_0_T74->SetName("DELTA_H_B_0_T74");DELTA_H_B_0_T74->SetTitle("DELTA_H_B_0_T74");
+  TGraphErrors* DELTA_H_B_0_T72 = (TGraphErrors*)f_72->Get("DELTA_H_B_0");DELTA_H_B_0_T72->SetName("DELTA_H_B_0_T72");DELTA_H_B_0_T72->SetTitle("DELTA_H_B_0_T72");
+  TGraphErrors* DELTA_H_B_0_T73 = (TGraphErrors*)f_73->Get("DELTA_H_B_0");DELTA_H_B_0_T73->SetName("DELTA_H_B_0_T73");DELTA_H_B_0_T73->SetTitle("DELTA_H_B_0_T73");
+  TGraphErrors* DELTA_H_B_0_T75 = (TGraphErrors*)f_75->Get("DELTA_H_B_0");DELTA_H_B_0_T75->SetName("DELTA_H_B_0_T75");DELTA_H_B_0_T75->SetTitle("DELTA_H_B_0_T75");
+  TGraphErrors* DELTA_H_B_0_T77 = (TGraphErrors*)f_77->Get("DELTA_H_B_0");DELTA_H_B_0_T77->SetName("DELTA_H_B_0_T77");DELTA_H_B_0_T77->SetTitle("DELTA_H_B_0_T77");
+  TGraphErrors* DELTA_H_B_0_T79 = (TGraphErrors*)f_79->Get("DELTA_H_B_0");DELTA_H_B_0_T79->SetName("DELTA_H_B_0_T79");DELTA_H_B_0_T79->SetTitle("DELTA_H_B_0_T79");
+  TGraphErrors* DELTA_H_B_0_T81 = (TGraphErrors*)f_81->Get("DELTA_H_B_0");DELTA_H_B_0_T81->SetName("DELTA_H_B_0_T81");DELTA_H_B_0_T81->SetTitle("DELTA_H_B_0_T81");
+  TGraphErrors* DELTA_H_B_0_T795 = (TGraphErrors*)f_795->Get("DELTA_H_B_0");DELTA_H_B_0_T795->SetName("DELTA_H_B_0_T795");DELTA_H_B_0_T795->SetTitle("DELTA_H_B_0_T795");
+
+  // DELTA_H_B_0_T77->SetMarkerStyle(20);DELTA_H_B_0_T77->SetMarkerColor(1);         DELTA_H_B_0_T77->SetMarkerSize(2);
+  DELTA_H_B_0_T80->SetMarkerStyle(20);DELTA_H_B_0_T80->SetMarkerColor(9);         DELTA_H_B_0_T80->SetMarkerSize(2);
+  DELTA_H_B_0_T82->SetMarkerStyle(20);DELTA_H_B_0_T82->SetMarkerColor(2);         DELTA_H_B_0_T82->SetMarkerSize(2);
+  DELTA_H_B_0_T84->SetMarkerStyle(20);DELTA_H_B_0_T84->SetMarkerColor(4);         DELTA_H_B_0_T84->SetMarkerSize(2);
+  DELTA_H_B_0_T86->SetMarkerStyle(20);DELTA_H_B_0_T86->SetMarkerColor(8);         DELTA_H_B_0_T86->SetMarkerSize(2);
+  DELTA_H_B_0_T88->SetMarkerStyle(20);DELTA_H_B_0_T88->SetMarkerColor(kOrange-2); DELTA_H_B_0_T88->SetMarkerSize(2);
+  DELTA_H_B_0_T90->SetMarkerStyle(20);DELTA_H_B_0_T90->SetMarkerColor(11);        DELTA_H_B_0_T90->SetMarkerSize(2);
+  DELTA_H_B_0_T78->SetMarkerStyle(20);DELTA_H_B_0_T78->SetMarkerColor(7);         DELTA_H_B_0_T78->SetMarkerSize(2);
+  DELTA_H_B_0_T76->SetMarkerStyle(20);DELTA_H_B_0_T76->SetMarkerColor(6);         DELTA_H_B_0_T76->SetMarkerSize(2);
+  DELTA_H_B_0_T74->SetMarkerStyle(20);DELTA_H_B_0_T74->SetMarkerColor(5);         DELTA_H_B_0_T74->SetMarkerSize(2);
+  DELTA_H_B_0_T72->SetMarkerStyle(20);DELTA_H_B_0_T72->SetMarkerColor(12);        DELTA_H_B_0_T72->SetMarkerSize(2);
+  DELTA_H_B_0_T73->SetMarkerStyle(20);DELTA_H_B_0_T73->SetMarkerColor(3);         DELTA_H_B_0_T73->SetMarkerSize(2);
+  DELTA_H_B_0_T75->SetMarkerStyle(20);DELTA_H_B_0_T75->SetMarkerColor(13);        DELTA_H_B_0_T75->SetMarkerSize(2);
+  DELTA_H_B_0_T77->SetMarkerStyle(20);DELTA_H_B_0_T77->SetMarkerColor(3);         DELTA_H_B_0_T77->SetMarkerSize(2);
+  DELTA_H_B_0_T79->SetMarkerStyle(20);DELTA_H_B_0_T79->SetMarkerColor(13);        DELTA_H_B_0_T79->SetMarkerSize(2);
+  DELTA_H_B_0_T81->SetMarkerStyle(20);DELTA_H_B_0_T81->SetMarkerColor(3);         DELTA_H_B_0_T81->SetMarkerSize(2);
+  DELTA_H_B_0_T795->SetMarkerStyle(20);DELTA_H_B_0_T795->SetMarkerColor(13);        DELTA_H_B_0_T795->SetMarkerSize(2);
   ////////////////////////////////////////////////
   /// Single pixel graphs //////
   //////////////////////////////
@@ -523,12 +602,12 @@ void Analyse_ScanTune(){
   err_DELTA_LSP_T0[0]=DELTA_LSP_T0_T90->GetErrorX(n);
 
   
-  TGraphErrors *TGE_DELTA_LSP_T0 = new TGraphErrors(16,TUNES,DELTA_LSP_T0,err_TUNES,err_DELTA_LSP_T0);TGE_DELTA_LSP_T0->SetName("DELTA_LSP_T0 che 4"); TGE_DELTA_LSP_T0->SetTitle(Form("T_{0} %d",n)); 
+  TGraphErrors *TGE_DELTA_LSP_T0 = new TGraphErrors(16,TUNES,DELTA_LSP_T0,err_TUNES,err_DELTA_LSP_T0);TGE_DELTA_LSP_T0->SetName("DELTA_LSP_T0 che 4"); TGE_DELTA_LSP_T0->SetTitle(Form("T_{L}^{SP}-T_{0} ch %d",n)); 
   TGE_DELTA_LSP_T0->SetMarkerStyle(20);TGE_DELTA_LSP_T0->SetMarkerColor(4);         TGE_DELTA_LSP_T0->SetMarkerSize(2);
   TCanvas* c_DELTA_LSP_T0_CH = new TCanvas("c_DELTA_LSP_T0_CH","c_DELTA_LSP_T0_CH");
   TGE_DELTA_LSP_T0->Draw("AP");
   TGE_DELTA_LSP_T0->GetXaxis()->SetTitle("Laser Tune");
-  TGE_DELTA_LSP_T0->GetYaxis()->SetTitle("T_{0} [ns]");
+  TGE_DELTA_LSP_T0->GetYaxis()->SetTitle("T_{L}^{SP}-T_{0} [ns]");
   gPad->BuildLegend();
   gPad->Update();
   gPad->SetGridy();
@@ -580,16 +659,123 @@ void Analyse_ScanTune(){
   err_DELTA_HSP_LSP[0]=DELTA_HSP_LSP_T90->GetErrorX(n);
 
   
-  TGraphErrors *TGE_DELTA_HSP_LSP = new TGraphErrors(16,TUNES,DELTA_HSP_LSP,err_TUNES,err_DELTA_HSP_LSP);TGE_DELTA_HSP_LSP->SetName("DELTA_HSP_LSP che 4"); TGE_DELTA_HSP_LSP->SetTitle(Form("T_{0} %d",n)); 
+  TGraphErrors *TGE_DELTA_HSP_LSP = new TGraphErrors(16,TUNES,DELTA_HSP_LSP,err_TUNES,err_DELTA_HSP_LSP);TGE_DELTA_HSP_LSP->SetName("DELTA_HSP_LSP che 4"); TGE_DELTA_HSP_LSP->SetTitle(Form("T_{H}^{SP}-T_{L}^{SP} ch %d",n)); 
   TGE_DELTA_HSP_LSP->SetMarkerStyle(20);TGE_DELTA_HSP_LSP->SetMarkerColor(4);         TGE_DELTA_HSP_LSP->SetMarkerSize(2);
   TCanvas* c_DELTA_HSP_LSP_CH = new TCanvas("c_DELTA_HSP_LSP_CH","c_DELTA_HSP_LSP_CH");
   TGE_DELTA_HSP_LSP->Draw("AP");
   TGE_DELTA_HSP_LSP->GetXaxis()->SetTitle("Laser Tune");
-  TGE_DELTA_HSP_LSP->GetYaxis()->SetTitle("T_{0} [ns]");
+  TGE_DELTA_HSP_LSP->GetYaxis()->SetTitle("T_{H}^{SP}-T_{L}^{SP} [ns]");
   gPad->BuildLegend();
   gPad->Update();
   gPad->SetGridy();
 
 
   
+  
+  Double_t SIGMA_H_B_0_au;
+  float SIGMA_H_B_0[16];
+  float err_SIGMA_H_B_0[16];
+
+  
+  SIGMA_H_B_0_T81->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[15]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T795->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[14]=SIGMA_H_B_0_au;   
+  SIGMA_H_B_0_T79->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[13]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T77->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[12]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T75->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[11]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T72->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[10]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T73->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[9]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T74->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[8]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T76->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[7]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T78->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[6]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T80->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[5]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T82->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[4]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T84->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[3]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T86->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[2]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T88->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[1]=SIGMA_H_B_0_au;
+  SIGMA_H_B_0_T90->GetPoint(n,x_au,SIGMA_H_B_0_au); SIGMA_H_B_0[0]=SIGMA_H_B_0_au;
+
+
+  err_SIGMA_H_B_0[15]=SIGMA_H_B_0_T81->GetErrorX(n);
+  err_SIGMA_H_B_0[14]=SIGMA_H_B_0_T795->GetErrorX(n);
+  err_SIGMA_H_B_0[13]=SIGMA_H_B_0_T79->GetErrorX(n);
+  err_SIGMA_H_B_0[12]=SIGMA_H_B_0_T77->GetErrorX(n);
+  err_SIGMA_H_B_0[11]=SIGMA_H_B_0_T75->GetErrorX(n);
+  err_SIGMA_H_B_0[10]=SIGMA_H_B_0_T72->GetErrorX(n);
+  err_SIGMA_H_B_0[9]=SIGMA_H_B_0_T73->GetErrorX(n);
+  err_SIGMA_H_B_0[8]=SIGMA_H_B_0_T74->GetErrorX(n);
+  err_SIGMA_H_B_0[7]=SIGMA_H_B_0_T76->GetErrorX(n);
+  err_SIGMA_H_B_0[6]=SIGMA_H_B_0_T78->GetErrorX(n);
+  err_SIGMA_H_B_0[5]=SIGMA_H_B_0_T80->GetErrorX(n);
+  err_SIGMA_H_B_0[4]=SIGMA_H_B_0_T82->GetErrorX(n);
+  err_SIGMA_H_B_0[3]=SIGMA_H_B_0_T84->GetErrorX(n);
+  err_SIGMA_H_B_0[2]=SIGMA_H_B_0_T86->GetErrorX(n);
+  err_SIGMA_H_B_0[1]=SIGMA_H_B_0_T88->GetErrorX(n);
+  err_SIGMA_H_B_0[0]=SIGMA_H_B_0_T90->GetErrorX(n);
+
+  
+  TGraphErrors *TGE_SIGMA_H_B_0 = new TGraphErrors(16,TUNES,SIGMA_H_B_0,err_TUNES,err_SIGMA_H_B_0);TGE_SIGMA_H_B_0->SetName("SIGMA_H_B_0 che 4"); TGE_SIGMA_H_B_0->SetTitle(Form("#delta T_{0} ch %d",n)); 
+  TGE_SIGMA_H_B_0->SetMarkerStyle(20);TGE_SIGMA_H_B_0->SetMarkerColor(4);         TGE_SIGMA_H_B_0->SetMarkerSize(2);
+  TCanvas* c_SIGMA_H_B_0_CH = new TCanvas("c_SIGMA_H_B_0_CH","c_SIGMA_H_B_0_CH");
+  TGE_SIGMA_H_B_0->Draw("AP");
+  TGE_SIGMA_H_B_0->GetXaxis()->SetTitle("Laser Tune");
+  TGE_SIGMA_H_B_0->GetYaxis()->SetTitle("#delta T_{0} [ns]");
+  gPad->BuildLegend();
+  gPad->Update();
+  gPad->SetGridy();
+
+
+
+  
+  
+  Double_t DELTA_H_B_0_au;
+  float DELTA_H_B_0[16];
+  float err_DELTA_H_B_0[16];
+
+  
+  DELTA_H_B_0_T81->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[15]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T795->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[14]=DELTA_H_B_0_au;   
+  DELTA_H_B_0_T79->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[13]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T77->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[12]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T75->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[11]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T72->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[10]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T73->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[9]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T74->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[8]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T76->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[7]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T78->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[6]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T80->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[5]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T82->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[4]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T84->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[3]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T86->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[2]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T88->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[1]=DELTA_H_B_0_au;
+  DELTA_H_B_0_T90->GetPoint(n,x_au,DELTA_H_B_0_au); DELTA_H_B_0[0]=DELTA_H_B_0_au;
+
+
+  err_DELTA_H_B_0[15]=DELTA_H_B_0_T81->GetErrorX(n);
+  err_DELTA_H_B_0[14]=DELTA_H_B_0_T795->GetErrorX(n);
+  err_DELTA_H_B_0[13]=DELTA_H_B_0_T79->GetErrorX(n);
+  err_DELTA_H_B_0[12]=DELTA_H_B_0_T77->GetErrorX(n);
+  err_DELTA_H_B_0[11]=DELTA_H_B_0_T75->GetErrorX(n);
+  err_DELTA_H_B_0[10]=DELTA_H_B_0_T72->GetErrorX(n);
+  err_DELTA_H_B_0[9]=DELTA_H_B_0_T73->GetErrorX(n);
+  err_DELTA_H_B_0[8]=DELTA_H_B_0_T74->GetErrorX(n);
+  err_DELTA_H_B_0[7]=DELTA_H_B_0_T76->GetErrorX(n);
+  err_DELTA_H_B_0[6]=DELTA_H_B_0_T78->GetErrorX(n);
+  err_DELTA_H_B_0[5]=DELTA_H_B_0_T80->GetErrorX(n);
+  err_DELTA_H_B_0[4]=DELTA_H_B_0_T82->GetErrorX(n);
+  err_DELTA_H_B_0[3]=DELTA_H_B_0_T84->GetErrorX(n);
+  err_DELTA_H_B_0[2]=DELTA_H_B_0_T86->GetErrorX(n);
+  err_DELTA_H_B_0[1]=DELTA_H_B_0_T88->GetErrorX(n);
+  err_DELTA_H_B_0[0]=DELTA_H_B_0_T90->GetErrorX(n);
+
+  
+  TGraphErrors *TGE_DELTA_H_B_0 = new TGraphErrors(16,TUNES,DELTA_H_B_0,err_TUNES,err_DELTA_H_B_0);TGE_DELTA_H_B_0->SetName("DELTA_H_B_0 che 4"); TGE_DELTA_H_B_0->SetTitle(Form("#Delta T ch %d",n)); 
+  TGE_DELTA_H_B_0->SetMarkerStyle(20);TGE_DELTA_H_B_0->SetMarkerColor(4);         TGE_DELTA_H_B_0->SetMarkerSize(2);
+  TCanvas* c_DELTA_H_B_0_CH = new TCanvas("c_DELTA_H_B_0_CH","c_DELTA_H_B_0_CH");
+  TGE_DELTA_H_B_0->Draw("AP");
+  TGE_DELTA_H_B_0->GetXaxis()->SetTitle("Laser Tune");
+  TGE_DELTA_H_B_0->GetYaxis()->SetTitle("#Delta T [ns]");
+  gPad->BuildLegend();
+  gPad->Update();
+  gPad->SetGridy();
+
 }
